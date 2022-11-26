887fe0b0f07ade68a666ed7b6657c078bee3324d
Hovig Kalayjian <hovigkalayjian@gmail.com>
2022-11-26 18:36:43 -0500

    trying another way to fethc

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 function fetchweather(queryURL) {
   var APIKey = "46d767d8cdd700f17ec41548fc1afb0f";
 
-  var city = "Miami";
-
-  var queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}`;
+  var city = "Toronto";
+  var lat = 43.653225;
+  var lon = -79.383186;
+  var queryURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${APIKey}`;
 
   fetch(queryURL)
     .then((response) => response.json())
-    .then((data) => this.displayWeather(data));
+    .then((data) => console.log(data));
 }
 
-function displayWeather(data) {
-  const name = data;
-  const { icon, description } = data.weather[0];
-  const { temp, humidity } = data.main;
-  const speed = data.wind;
+// function displayWeather(data) {
+//   const name = data;
+//   const { icon, description } = data.weather[0];
+//   const { temp, humidity } = data.main;
+//   const speed = data.wind;
 
-  console.log(name, icon, description, temp, humidity, speed);
-}
+//   console.log(name, icon, description, temp, humidity, speed);
+// }
 
 fetchweather();
